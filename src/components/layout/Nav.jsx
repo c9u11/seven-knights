ae3ecbc28a0a954a263a64604382b59d2b14716e
c9u11 <tjrdud6412@naver.com>
2025-05-27 23:10:25 +0900

    🧑‍💻 Add routes list contant

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -7,36 +7,25 @@ import {
 } from "@chakra-ui/react";
 import { Link, useLocation } from "react-router-dom";
 import { ColorModeButton } from "@/components/ui/color-mode";
-import {
-  ROUTES_PATH_FARMING_CALCULATOR,
-  ROUTES_PATH_ROOT
-} from "@/constants/routes";
+import { ROUTES_LIST } from "@/constants/routes";
 
 export default function Nav() {
   const location = useLocation();
   return (
     <HStack justifyContent="space-between">
       <nav>
-        <HStack>
-          <ChakraLink
-            asChild
-            color={
-              location.pathname === ROUTES_PATH_ROOT ? "primary.500" : undefined
-            }
-          >
-            <Link to={ROUTES_PATH_ROOT}>홈</Link>
-          </ChakraLink>
-          <Separator orientation="vertical" />
-          <ChakraLink
-            asChild
-            color={
-              location.pathname === ROUTES_PATH_FARMING_CALCULATOR
-                ? "primary.500"
-                : undefined
-            }
-          >
-            <Link to={ROUTES_PATH_FARMING_CALCULATOR}>쫄작 계산기</Link>
-          </ChakraLink>
+        <HStack gap={4}>
+          {ROUTES_LIST.map((route) => (
+            <ChakraLink
+              key={route.path}
+              asChild
+              color={
+                location.pathname === route.path ? "primary.500" : undefined
+              }
+            >
+              <Link to={route.path}>{route.name}</Link>
+            </ChakraLink>
+          ))}
         </HStack>
       </nav>
       <ColorModeButton />
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,14 @@
 export const ROUTES_BASENAME = import.meta.env.BASE_URL;
 export const ROUTES_PATH_ROOT = '/';
 export const ROUTES_PATH_FARMING_CALCULATOR = "/farming-calculator";
+
+export const ROUTES_LIST = [
+  {
+    name: '홈',
+    path: ROUTES_PATH_ROOT,
+  },
+  {
+    name: '쫄작 계산기',
+    path: ROUTES_PATH_FARMING_CALCULATOR,
+  }
+]
